b9a6c6ebf2859a65e24c70f322d7ae2db45c980f
Vonschell <vonschell02@gmail.com>
2025-01-23 18:33:25 +0900

    Updated the quotes

--- src/App.js
+++ src/App.js
@@ -12,29 +12,46 @@ function App() {
   const quotesUrl =
     "https://gist.githubusercontent.com/skillcrush-curriculum/6365d193df80174943f6664c7c6dbadf/raw/1f1e06df2f4fc3c2ef4c30a3a4010149f270c0e0/quotes.js";
   const categories = ["All", "Leadership", "Empathy", "Motivation", "Learning", "Success", "Empowerment"];
-  
-  const fetchQuotes = async () => {
-      try {
-        setLoading(true);
-        const response = await fetch(quotesUrl);
-        const results = await response.json();
-        setQuotes(results);
-      } catch (error) {
-        console.log("There was an error", error);
-      }
-      setLoading(false);
-    };
 
-    useEffect(() => {
-      fetchQuotes();
-    }, []);
+  const fetchQuotes = async () => {
+    try {
+      setLoading(true);
+      const response = await fetch(quotesUrl);
+      const results = await response.json();
+      setQuotes(results);
+    } catch (error) {
+      console.log("There was an error", error);
+    }
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    fetchQuotes();
+  }, []);
+
+  const filteredQuotes = category !== "All" ? quotes.filter((quote) => quote.categories.includes(category)) : quotes;
+
+  const handleCategoryChange = (e) => {
+    setCategory(e.target.value);
+  };
 
   return (
     <div className='App'>
       <Header />
-      <main>{loading ? <Loader /> : <Quotes quotes={quotes} categories={categories} category={category}/>}</main>
+      <main>
+        {loading ? (
+          <Loader />
+        ) : (
+          <Quotes filteredQuotes={filteredQuotes}
+            categories={categories}
+            category={category}
+            handleCategoryChange={handleCategoryChange}
+          />
+        )}
+      </main>
       <Footer />
     </div>
   );
 }
+
 export default App;
