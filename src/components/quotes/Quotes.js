73546f903640df1bf74a429e972396c1eb9ec971
Vonschell <vonschell02@gmail.com>
2025-01-23 10:06:06 +0900

    Creating a QuoteCard component

---
+++ src/components/quotes/Quotes.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Quotes () {
+    return (
+        <section className='all-quotes'>
+            <div className='quotes wrapper'></div>
+        </section>
+    );
+}
+
+export default Quotes
