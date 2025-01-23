e30cd20c0d0934b694af3ae8da3b046f870cc9b3
Vonschell <vonschell02@gmail.com>
2025-01-23 17:42:41 +0900

    Creating state for categories

--- src/App.js
+++ src/App.js
@@ -8,10 +8,12 @@ import "./App.css";
 function App() {
   const [quotes, setQuotes] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [category, setCategory] = useState("All");
   const quotesUrl =
     "https://gist.githubusercontent.com/skillcrush-curriculum/6365d193df80174943f6664c7c6dbadf/raw/1f1e06df2f4fc3c2ef4c30a3a4010149f270c0e0/quotes.js";
-
-    const fetchQuotes = async () => {
+  const categories = ["All", "Leadership", "Empathy", "Motivation", "Learning", "Success", "Empowerment"];
+  
+  const fetchQuotes = async () => {
       try {
         setLoading(true);
         const response = await fetch(quotesUrl);
