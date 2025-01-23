e321364b2ea3db9822d180c0ee80e6531cc03d64
Vonschell <vonschell02@gmail.com>
2025-01-23 09:45:58 +0900

    Fetching quotes by creating async function with a try/catch block inside it

--- src/App.js
+++ src/App.js
@@ -9,6 +9,22 @@ function App() {
   const quotesUrl =
     "https://gist.githubusercontent.com/skillcrush-curriculum/6365d193df80174943f6664c7c6dbadf/raw/1f1e06df2f4fc3c2ef4c30a3a4010149f270c0e0/quotes.js";
 
+    const fetchQuotes = async () => {
+      try {
+        setLoading(true);
+        const response = await fetch(quotesUrl);
+        const results = await response.json();
+        setQuotes(results);
+      } catch (error) {
+        console.log("There was an error", error);
+      }
+      setLoading(false);
+    };
+
+    useEffect(() => {
+      fetchQuotes();
+    }, []);
+    
   return (
     <div className='App'>
       <Header />
