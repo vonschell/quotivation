6ee9f87f93e959fb5eab7700c737b6584b1fc41d
Vonschell <vonschell02@gmail.com>
2025-01-23 09:36:03 +0900

    created the state variables

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Footer from "./components/Footer";
 import "./App.css";
 
 function App() {
+  const [quotes, setQuotes] = useState([]);
+  const [loading, setLoading] = useState(false)
   const quotesUrl =
     "https://gist.githubusercontent.com/skillcrush-curriculum/6365d193df80174943f6664c7c6dbadf/raw/1f1e06df2f4fc3c2ef4c30a3a4010149f270c0e0/quotes.js";
 
