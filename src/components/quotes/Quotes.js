853b6230813076b8cd54df6aeb41bdf5edcdc815
Vonschell <vonschell02@gmail.com>
2025-01-23 18:42:57 +0900

    Fixing a few mistakes and updating the quotes component

--- src/components/quotes/Quotes.js
+++ src/components/quotes/Quotes.js
@@ -2,15 +2,15 @@ import React from "react";
 import QuoteCard from "./QuoteCard";
 import CategoryForm from "./CategoryForm";
 
-function Quotes({ quotes, categories, category }) {
+function Quotes({ filteredQuotes, category, categories, handleCategoryChange }) {
     return (
         <section className='all-quotes'>
             <div className='quotes wrapper'>
                 <div className='category-header'>
-                <p>Browse through your collection of quotes.</p>
-                <CategoryForm categories={categories} selectedCategory={category} />
+                    <p>Browse through your collection of quotes.</p>
+                    <CategoryForm categories={categories} category={category} handleCategoryChange={handleCategoryChange} />
                 </div>
-                {quotes.map((quote) => (
+                {filteredQuotes.map((quote) => (
                     <QuoteCard key={quote.id} quote={quote} />
                 ))}
             </div>
