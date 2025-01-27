e10757cf3085ec27698b69b3d092c0284ad14daf
Vonschell <vonschell02@gmail.com>
2025-01-27 09:06:09 +0900

    Displaying favorite quotes in JSON format

--- src/App.js
+++ src/App.js
@@ -58,6 +58,21 @@ function App() {
     <div className='App'>
       <Header />
       <main>
+        <section className='favorite-quotes'>
+          <div className='wrapper quotes'>
+            <h3>Top 3 favorite quotes</h3>
+            {favoriteQuotes.length > 0 && JSON.stringify(favoriteQuotes)}
+
+            <div className='favorite-quotes-description'>
+              <p>
+                You can add up to three favorites by selecting from the options below.
+                <br />
+                Once you choose, they will appear here. 
+                </p>
+            </div>
+          </div>
+        </section>
+
         {loading ? (
           <Loader />
         ) : (
