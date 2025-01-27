001a056f13d4a1d5877c08eb0b11ea298a25f1b4
Vonschell <vonschell02@gmail.com>
2025-01-27 10:23:33 +0900

    Adding onClick so that I can actually delete a quote

--- src/components/quotes/FavoriteQuoteCard.js
+++ src/components/quotes/FavoriteQuoteCard.js
@@ -3,7 +3,9 @@ import React from "react";
 function FavoriteQuoteCard({ quote, removeFromFavorites }) {
     return (
         <li className="quote-card">
-            <span className='close-quote'>x</span>
+            <span className='close-quote' onClick={() => removeFromFavorites(quote.id)}>
+                x
+            </span>
             <h3>{quote.author}</h3>
             <p>{quote.author}</p>
         </li>
